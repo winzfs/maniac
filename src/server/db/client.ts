ed19c4feda104a86fed900e2bbb42974f4c9316b
winzfs <jazzhjm@gmail.com>
2026-05-28 23:35:49 +0900

    fix: avoid global D1 types in db client boundary

--- src/server/db/client.ts
+++ src/server/db/client.ts
@@ -1,12 +1,14 @@
 import { drizzle } from "drizzle-orm/d1";
 import * as schema from "./schema";
 
+type D1LikeDatabase = Parameters<typeof drizzle>[0];
+
 export type ManiacDatabase = ReturnType<typeof createDb>;
 
-export function createDb(database: D1Database) {
+export function createDb(database: D1LikeDatabase) {
   return drizzle(database, { schema });
 }
 
 export type DatabaseProvider = {
-  DB: D1Database;
+  DB: D1LikeDatabase;
 };
