e5d74f8993a6925a127befa74bbc3ec2b7cb0d4f
winzfs <jazzhjm@gmail.com>
2026-05-29 19:17:18 +0900

    chore: add mock user production guard

--- functions/_shared/dev-user.ts
+++ functions/_shared/dev-user.ts
@@ -1,1 +1,10 @@
 export const MOCK_USER_ID = "dev_user_maniac";
+export const MOCK_USER_PRODUCTION_ERROR = "Login is required in production.";
+
+export type MockUserEnv = {
+  APP_ENV?: string;
+};
+
+export function isMockUserWriteBlocked(env: MockUserEnv) {
+  return env.APP_ENV === "production";
+}
