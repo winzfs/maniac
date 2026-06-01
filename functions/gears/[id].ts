7e1384c8000ca15a5b389474b1546c5ced2180af
winzfs <jazzhjm@gmail.com>
2026-06-01 10:37:28 +0900

    Add clean gear URL redirect

---
+++ functions/gears/[id].ts
@@ -0,0 +1,17 @@
+/// <reference types="@cloudflare/workers-types" />
+
+function paramValue(params: Record<string, string | string[]>, key: string) {
+  const value = params[key];
+  return Array.isArray(value) ? value[0] : value;
+}
+
+export const onRequestGet: PagesFunction = async ({ params, request }) => {
+  const id = paramValue(params, "id");
+  if (!id) return new Response("Gear id is required.", { status: 400 });
+
+  const url = new URL(request.url);
+  url.pathname = "/garage/view/";
+  url.search = `?id=${encodeURIComponent(id)}`;
+
+  return Response.redirect(url.toString(), 302);
+};
